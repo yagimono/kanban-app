ffa7232687541d5325be335b500b0cfb5886e7d3
yagimono <yagi@monosense.co.jp>
2019-02-22 09:26:27 +0900

    stateの初期化

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,20 @@ import mutations from './mutations'
 
 Vue.use(Vuex)
 
+// 状態`Auth`と状態`Board`をVuexのstateで一元管理できるよう定義する
+
+const state = {
+  auth: { // 状態`Auth`
+    token: null, // `token`はnullで初期化
+    userId: null // `userId`はnullで初期化
+  },
+  board: { // 状態`Auth`
+    lists: [] // `taskList`は空で初期化
+  }
+}
+
 export default new Vuex.Store({
+  state, // 定義したstateを`state`オプションに指定
   getters,
   actions,
   mutations,
