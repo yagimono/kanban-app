db199aa2c7c52c6cc19324262d013decf62a02bd
yagimono <yagi@monosense.co.jp>
2019-03-08 00:14:44 +0900

    エラーハンドリングの実装

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import 'es6-promise/auto' // プロミスをポリフィルする
 import App from './App'
+import ErrorBoundary from './ErrorBoundary.vue' // エラーを細くするコンポーネント
 import router from './router'
 import store from './store' // Vuexのストアインスタンスをインポート
 
 Vue.config.productionTip = false
+
+// ErrorBoundaryコンポーネントのインストール
+Vue.component(ErrorBoundary.name, ErrorBoundary)
+
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error('errorHandler err:', err)
+  console.error('errorHandler vm:', vm)
+  console.error('errorHandler info:', info)
+}
 
 /* eslint-disable no-new */
 new Vue({
